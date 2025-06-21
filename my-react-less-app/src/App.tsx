07559714df05ad78511c14bd2e482dce05f9135a
idodidodi <idodidodi@gmail.com>
2025-06-21 18:23:54 +0300

    save add and delete works

--- my-react-less-app/src/App.tsx
+++ my-react-less-app/src/App.tsx
@@ -1,37 +1,67 @@
 import './App.less'
 
-import { useState, useEffect, type MouseEvent } from 'react'
+import { useState, useEffect, type MouseEvent, useRef } from 'react'
 import type { ProductData } from './models'
 import { dummyProducts } from './dummy-data'
 import { Product, ProductDetails } from './components/product';
 
 function App() {
   const [products, setProducts] = useState<ProductData[]>([]);
+  const firstInit = useRef<boolean>(null);
   const [selectedItemIdx, setSelectedItemIdx] = useState<number>(-1);
+  const [createNewItem, setCreateNewItem] = useState<boolean>(false);
 
-  // const updateProducts = ():void=>{
-  //   setProducts(()=>{
-  //     if (products.length === 0) return [];
-  //     const nextState =  products.current.filter(item => item.state === "approved");
-  //     setSelectedItemIdx(nextState.findIndex(item=>item.state === "approved"));
-  //     return nextState;
-  //   })
-  // }
   useEffect(() => {
-    setProducts([...dummyProducts]);
-  }, [])
+    if (firstInit.current == null) {
+      setProducts(() => {
+        const nextState = [...dummyProducts];
+        setSelectedItemIdx(nextState.findIndex(item => item.state === "approved"));
+        return nextState;
+      }
+      );
+      firstInit.current = false;
+    } else {
+      if (selectedItemIdx === -1) {
+        setSelectedItemIdx(products.findIndex(item => item.state === "approved"));
+      }
+    }
+  }, [products])
 
-  const showProdcutDetails = (): boolean => selectedItemIdx > -1 && products[selectedItemIdx].state !== "deleted";
-  // const findItemIdx = (item:ProductData, id:number):boolean=>item.id === id;
-  // const getIdx = (id: number):number=> products.findIndex((_item)=>findItemIdx(_item, id));
-  console.log(products)
+  const showProdcutDetails = (): boolean => createNewItem || (selectedItemIdx > -1 && products[selectedItemIdx]?.state !== "deleted");
 
+  const getNewEmptyItem = (): ProductData => {
+    return { name: "New item name", id: products.length + 1, price: 0, creationDate: new Date(), state: "pending" };
+
+  }
+  const addItem = () => {
+    setCreateNewItem(true);
+  }
+
+  const saveItem = (newItem: ProductData): void => {
+    setProducts(prev => {
+      const nextState = [...prev];
+      const idx = nextState.findIndex(item=>item.id === newItem.id);
+      if (idx === -1) {
+        nextState.push({...newItem, state:"approved"});
+        setCreateNewItem(false);
+      } else {
+        nextState[idx].description = newItem.description;
+        nextState[idx].price = newItem.price;
+        nextState[idx].name = newItem.name;
+      }
+      return nextState;
+    })
+
+  }
   return (
     <div className='store-container'>
       <div className="header">MY STORE</div>
       <div className="body">
         <div className="actions">
-          <button className="add">+Add</button>
+          <button
+            className="add"
+            onClick={addItem}
+          >+Add</button>
           <div className="search-products">
             <div className="maginifying-glass">Add maginifying glass</div>
             <input type="text" title='Search producs' defaultValue="Search producs" />
@@ -41,28 +71,30 @@ function App() {
         <div className="products">
           <div className="list">{
             products.
-            filter(item=>item.state === "approved").
-            map((item, idx) => <
-              Product
-              item={item}
-              key={item.id}
-              onSelect={() => {
-                setSelectedItemIdx(idx);
-              }}
-              onDelete={(e: MouseEvent) => {
-                const _idx = products.findIndex(_item => _item.id === item.id);
-                if (_idx > - 1) {
-                  products[_idx].state = "deleted";
-                  setProducts([...products]);
-                }
-                setSelectedItemIdx(-1);
-                e.stopPropagation();
+              filter(item => item.state === "approved").
+              map((item, idx) => <
+                Product
+                item={item}
+                key={item.id}
+                onSelect={() => {
+                  setSelectedItemIdx(products.findIndex(_item => _item.id === item.id));
+                }}
+                onDelete={(e: MouseEvent) => {
+                  const _idx = products.findIndex(_item => _item.id === item.id);
+                  if (_idx > - 1) {
+                    products[_idx].state = "deleted";
+                    setProducts([...products]);
+                  }
+                  if (!createNewItem) {
+                    setSelectedItemIdx(-1);
+                  }
+                  e.stopPropagation();
 
-              }}
-            />)
+                }}
+              />)
           }</div>
           <div className="item-description">
-            {showProdcutDetails() && <ProductDetails item={products[selectedItemIdx]} />}
+            {showProdcutDetails() && <ProductDetails item={createNewItem ? getNewEmptyItem() : products[selectedItemIdx]} saveItem={saveItem} />}
           </div>
         </div>
         <div className="pagination"></div>
--- my-react-less-app/src/components/product.tsx
+++ my-react-less-app/src/components/product.tsx
@@ -1,11 +1,11 @@
 import './product.less'
 
-import { type JSX, type MouseEvent } from 'react';
+import { useEffect, useState, type JSX, type MouseEvent } from 'react';
 import type { ProductData } from '../models';
 
 
-export const Product = (props: { item: ProductData, onSelect:()=>void , onDelete:(e:MouseEvent)=>void}): JSX.Element => {
-    const {item, onSelect, onDelete} = props;
+export const Product = (props: { item: ProductData, onSelect: () => void, onDelete: (e: MouseEvent) => void }): JSX.Element => {
+    const { item, onSelect, onDelete } = props;
     const { name, description } = item;
     return (
         <div className='product'
@@ -25,28 +25,85 @@ export const Product = (props: { item: ProductData, onSelect:()=>void , onDelete
     );
 };
 
-export const ProductDetails = (props: { item: ProductData }): JSX.Element => {
-    const { name, description, price } = props.item;
+export const ProductDetails = (props: { item: ProductData, saveItem: (item: ProductData) => void }): JSX.Element => {
+    const { item, saveItem } = props;
+    const { name, description, price } = item;
+    const [savedItem, setSavedItem] = useState<ProductData>({ ...item });
+    const validateSavedItem = (savedItem.description == null || savedItem.description.length <= 200)
+        && savedItem.name.length > 0
+        && savedItem.name.length < 31
+        && savedItem.price > 0;
 
+    useEffect(() => {
+        setSavedItem({ ...item });
+    }, [item])
     return (
         <div className='product-details-container'>
             <div className="product-details-title">{name} details</div>
             <div className="body">
                 <ProductImage />
-                <TtileAndDataBox title='Name' text={name} />
-                <TtileAndDataBox title='Description' text={description || ''} />
-                <TtileAndDataBox title='Price' text={price} />
+                <TtileAndDataBox title='Name' text={name}
+                    onChange={(e) => {
+                        const input: string = e.target.value;
+
+                        setSavedItem(prev => {
+                            const nextState = { ...prev };
+                            nextState.name = input;
+                            return nextState;
+                        })
+                    }}
+                />
+                <TtileAndDataBox title='Description' text={description || ''}
+                    onChange={(e) => {
+                        const input: string = e.target.value;
+                        setSavedItem(prev => {
+                            const nextState = { ...prev };
+                            nextState.description = input;
+                            return nextState;
+                        })
+                    }}
+                />
+                <TtileAndDataBox title='Price' text={price} onChange={(e) => {
+                    const input: string = e.target.value;
+                    if (isNaN(Number(input))) {
+                        console.error({ input }, "is not a number")
+                        return;
+                    }
+                    setSavedItem(prev => {
+                        const nextState = { ...prev };
+                        nextState.price = Number(input);
+                        return nextState;
+                    })
+
+                }} />
+            </div>
+            <div className="add-button-wrapper">
+                <button className='save-button' onClick={() => saveItem(savedItem)} disabled={!validateSavedItem}>Save</button>
             </div>
         </div>
     );
 };
 
-const TtileAndDataBox = (props: { title: string; text: string | number }): JSX.Element => {
+const TtileAndDataBox = (props: { title: string; text: string | number, onChange: (e: any) => void }): JSX.Element => {
+    const [val, setVal] = useState<number | string>(props.text);
+    useEffect(()=>{
+        setVal(props.text)
+    },[props.text])
+
+    const _onChangeInput = (e:any)=>{
+        if (val !== e.target.value) {
+            setVal(e.target.value);
+        }
+        props.onChange(e);
+    }
     return (
         <div className='box-wrapper'>
             <div className="title">{props.title}</div>
-            <div className="bordered-box">
+            {/* <div className="bordered-box">
                 {props.text}
+            </div> */}
+            <div className="input-bordered-box">
+                <input type='text' value={val} onChange={_onChangeInput}></input>
             </div>
         </div>
     );
